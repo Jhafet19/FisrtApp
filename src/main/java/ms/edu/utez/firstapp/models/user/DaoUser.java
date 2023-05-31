0ffffbd2a7889a29c805c9dae41e2d2ff82473b3
RodriguezGarciaJhafetJonatan <20223tn107@utez.edu.mx>
2023-05-31 11:26:59 -0600

    [update]nueva clase servlet

--- src/main/java/ms/edu/utez/firstapp/models/user/DaoUser.java
+++ src/main/java/ms/edu/utez/firstapp/models/user/DaoUser.java
@@ -1,13 +1,54 @@
 package ms.edu.utez.firstapp.models.user;
 
 import ms.edu.utez.firstapp.models.crud.DaoRepository;
+import ms.edu.utez.firstapp.utils.MySQLConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DaoUser implements DaoRepository<user> {
+
+    private Connection conn;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+
     @Override
     public List<user> findAll() {
-        return null;
+        List<user> users =new ArrayList<>();
+        try{
+            conn= new MySQLConnection().connect();
+            String query ="SELECT * FROM users;";
+            pstm = conn.prepareStatement(query);
+            rs= pstm.executeQuery();
+            while(rs.next()){
+                user user=new user();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setSurname(rs.getString("surname"));
+                user.setLastname(rs.getString("lastname"));
+                user.setBirthaday(rs.getString("birthanday"));
+                user.setStatus(rs.getString("status"));
+                user.setUsername(rs.getString("username"));
+                users.add(user);
+
+            }
+
+            //un query es una consulta y Statement
+
+        }catch(SQLException e){
+            Logger.getLogger(DaoUser.class.getName()).log(Level.SEVERE, "Error findAll"+e.getMessage());
+
+        }finally {
+            close();
+        }
+        //finally siempre se ejecuta no depende de nada
+        return users;
     }
 
     @Override
@@ -28,6 +69,15 @@ public class DaoUser implements DaoRepository<user> {
     @Override
     public boolean delate(Long id) {
         return false;
+    }
+    public void close(){
+    try {
+        if(conn != null)conn.close();
+        if(pstm !=null) pstm.close();
+        if(rs != null) rs.close();
+    }catch(SQLException e){
+
+        }
     }
     //Acceso a base de datos es el intermediario en la aplicación y la base de datos
 
